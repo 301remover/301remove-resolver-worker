d4833fd2311ca0fb312dba6f216a8cbd541bc1e6
Ben Halligan <bshalligan@gmail.com>
2020-01-14 23:33:54 +0000

    Tests and Rate Limiting

--- lib/resolver_worker.ex
+++ lib/resolver_worker.ex
@@ -1,18 +1,52 @@
 defmodule ResolverWorker do
+  use GenServer
+
   @moduledoc """
   Documentation for ResolverWorker.
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ResolverWorker.hello()
-      :world
-
   """
-  def hello do
-    :world
+
+  def get_loc(response) do
+    Enum.filter(response.headers, fn
+      {key, _} -> String.match?(key, ~r/\Alocation\z/i)
+    end)
+  end
+
+  @doc """
+  Handles a call back for tinyurl and bit.ly links
+  """
+  def tinyurl(server_pid, url) do
+    GenServer.call(server_pid, {:tiny, url})
+  end
+
+  def bitly(server_pid, url) do
+    GenServer.call(server_pid, {:bitly, url})
+  end
+
+  def handle_call({:tiny, short}) do
+    response = HTTPoison.get!(short)
+    location = get_loc(response)
+    loc = List.first(location)
+    url = elem(loc, 1)
+    {:reply, :ok, url}
+  end
+
+  def handle_call({:bitly, short}) do
+    response = HTTPoison.get!(short)
+    location = get_loc(response)
+    loc = List.first(location)
+    url = elem(loc, 1)
+    {:reply, :ok, url}
+  end
+
+  # GenServer init stuff
+  def start_link() do
+    GenServer.start_link(__MODULE__, [])
+  end
+
+  def init(_) do
+    {:ok, 1}
   end
 end
