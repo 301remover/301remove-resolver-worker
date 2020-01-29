718d312e9edd4499d3a1dae148a6a4d929097a62
Ben Hallgain <bshalligan@gmail.com>
2020-01-28 20:42:55 -0600

    added timestamps to state in resolve and removed puts

--- lib/resolver_worker/resolve.ex
+++ lib/resolver_worker/resolve.ex
@@ -22,6 +22,12 @@ defmodule ResolverWorker.Resolve do
     GenServer.call(__MODULE__, {:google, url})
   end
 
+  defp get_loc(response) do
+    Enum.filter(response.headers, fn
+      {key, _} -> String.match?(key, ~r/\Alocation\z/i)
+    end)
+  end
+
   defp get_full(url) do
     url
     |> HTTPoison.get!()
@@ -71,6 +77,7 @@ defmodule ResolverWorker.Resolve do
   def handle_call({:google, code}, _from, state) do
     url = get_full("https://goo.gl/" <> code)
     {:reply, {:ok, url}, increment_time(state, :google)}
+  end
 
   # GenServer init stuff
 
